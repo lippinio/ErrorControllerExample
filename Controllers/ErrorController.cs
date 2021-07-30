c8feb23c22360145e1e9aa291a3a082ba699a6ff
David Lips <david.lips@digitecgalaxus.ch>
2021-07-30 12:25:35 +0200

    initial commit

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using ErrorControllerExample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ErrorControllerExample.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Handle()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ErrorControllerExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        
+        public IActionResult Fancy()
+        {
+            var rand = new Random();
+            if (rand.Next() % 2 == 1)
+            {
+                throw new Exception();
+            }
+
+            return View();
+        }
+    }
+}
